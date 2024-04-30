f2ce374c97f65c78d63ff28405088e1a56a84623
Jim C K Flaten <jckf@jckf.no>
2024-04-30 14:34:27 +0200

    Run in microadmin dir.

--- Micro Admin.bat
+++ Micro Admin.bat
@@ -4,7 +4,9 @@ cls
 
 set PATH=%~dp0\php\php-8.0.27;%PATH%
 
-call php -d curl.cainfo="%~dp0\php\cacert.pem" client\eaw.php microadmin\microadmin.php
+cd microadmin
+
+call php -d curl.cainfo="%~dp0\php\cacert.pem" ..\client\eaw.php microadmin.php
 
 echo The script exited.
 pause
