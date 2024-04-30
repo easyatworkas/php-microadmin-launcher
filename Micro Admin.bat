b27556f3f666b65065aa2b54a546b071bf8b89d8
Jim C K Flaten <jckf@jckf.no>
2024-04-30 14:53:41 +0200

    Force INFO log level.

--- Micro Admin.bat
+++ Micro Admin.bat
@@ -6,6 +6,8 @@ set PATH=%~dp0\php\php-8.0.27;%PATH%
 
 cd microadmin
 
+set EAW_LOG_LEVEL=INFO
+
 call php -d curl.cainfo="%~dp0\php\cacert.pem" ..\client\eaw.php microadmin.php
 
 echo The script exited.
